2107e6f64fa8ba35e52e00d140bd713a3d15d00b
Nathaniel Saul <nat@saulgill.com>
2019-03-20 15:37:35 -0700

    move run_mapper to mappercore

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from mappercore import Server
-from helpers.func import run_mapper
+# from helpers.func import run_mapper
 
 # create server instance
 server = Server("Mapper Example")
 
-# register your backend function here
-server.register_function('run_mapper', run_mapper)
 
-# to run the server, please use:
+# to run the server in production, please use:
 #  gunicorn run:server
+
+# and comment out this line
+server.flask.run()
