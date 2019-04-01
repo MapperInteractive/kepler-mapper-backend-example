e817371b259f3c230cb0742e34734b760c53acb4
Yaodong Zhao <git@yaodong.org>
2019-03-31 23:39:53 -0600

    use kepler mapper without main.js

--- run.py
+++ run.py
@@ -1,25 +1,20 @@
 from os import path
+from sklearn import datasets
 
 from mappercore import Server
 from mappercore.conf.kepler_mapper import KeplerMapperConfig
-from sklearn import datasets
-import numpy as np
-
-import pandas as pd
-
-# create server instance
-server = Server("Mapper Example")
 
 # create kepler mapper config
-kepler_config = KeplerMapperConfig()
+conf = KeplerMapperConfig()
+
+# create server instance
+server = Server("Mapper Example", conf=conf)
 
 # get example data
-data = datasets.make_circles(n_samples=1000)[0]
-target = kepler_config.mapper.project(data)
+data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
+projected_data = conf.mapper.fit_transform(data, projection=[0,1]) # X-Y axis
 
-kepler_config.set_data(data)
-kepler_config.set_target(target)
-kepler_config.setup(server)
+conf.set_data(data).set_lens(projected_data)
 
 # to run the server, please use:
 #  gunicorn run:server
