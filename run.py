9a7ee715b423706423622b4f6bb876da2a6b0ac6
Nathaniel Saul <nat@saulgill.com>
2019-04-03 10:32:13 -0700

    user supplies name of json file for user side customization

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
-from os import path
 from sklearn import datasets
 
 from mappercore import Server
 from mappercore.conf.kepler_mapper import KeplerMapperConfig
 
-# create kepler mapper config
-conf = KeplerMapperConfig()
+# Load example data
+data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
 
-# create server instance
+# Create kepler mapper config
+conf = KeplerMapperConfig(data=data, config="config.json")
+
+# Create server instance
 server = Server("Mapper Example", conf=conf)
 
-# get example data
-data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
-projected_data = conf.mapper.fit_transform(data, projection=[0,1]) # X-Y axis
-
-conf.set_data(data).set_lens(projected_data)
-
 # to run the server, please use:
-#  gunicorn run:server
+# >>> gunicorn run:server
+# or 
+# >>> python run.py
+
+if __name__ == "__main__":
+    server.flask.run()
