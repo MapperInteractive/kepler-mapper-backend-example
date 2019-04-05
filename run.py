0d822419a28d114b0e13e979408cfa186164173e
Yaodong Zhao <git@yaodong.org>
2019-04-05 15:37:57 -0600

    upgrade mappercore

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from os import path
 from sklearn import datasets
 
 from mappercore import Server
@@ -12,9 +11,9 @@ server = Server("Mapper Example", conf=conf)
 
 # get example data
 data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
-projected_data = conf.mapper.fit_transform(data, projection=[0,1]) # X-Y axis
+projected_data = conf.mapper.fit_transform(data, projection=[0, 1])  # X-Y axis
 
 conf.set_data(data).set_lens(projected_data)
 
 # to run the server, please use:
-#  gunicorn run:server
+# gunicorn run:server
